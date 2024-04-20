81c2d9facc89f3347a5f97b937d31cf15d2d830d
FongMi <b8892778@gmail.com>
2024-04-20 22:16:50 +0800

    Optimize Interceptor

--- catvod/src/main/java/com/github/catvod/net/OkHttp.java
+++ catvod/src/main/java/com/github/catvod/net/OkHttp.java
@@ -6,6 +6,8 @@ import android.text.TextUtils;
 import androidx.collection.ArrayMap;
 
 import com.github.catvod.bean.Doh;
+import com.github.catvod.net.interceptor.ResponseInterceptor;
+import com.github.catvod.net.interceptor.RequestInterceptor;
 import com.github.catvod.utils.Path;
 
 import java.net.ProxySelector;
@@ -138,7 +140,7 @@ public class OkHttp {
     }
 
     private static OkHttpClient.Builder getBuilder() {
-        OkHttpClient.Builder builder = new OkHttpClient.Builder().cookieJar(new OkCookieJar()).addNetworkInterceptor(new OkInterceptor()).connectTimeout(TIMEOUT, TimeUnit.MILLISECONDS).readTimeout(TIMEOUT, TimeUnit.MILLISECONDS).writeTimeout(TIMEOUT, TimeUnit.MILLISECONDS).dns(dns()).hostnameVerifier((hostname, session) -> true).followRedirects(true).sslSocketFactory(getSSLContext().getSocketFactory(), trustAllCertificates());
+        OkHttpClient.Builder builder = new OkHttpClient.Builder().cookieJar(new OkCookieJar()).addInterceptor(new RequestInterceptor()).addNetworkInterceptor(new ResponseInterceptor()).connectTimeout(TIMEOUT, TimeUnit.MILLISECONDS).readTimeout(TIMEOUT, TimeUnit.MILLISECONDS).writeTimeout(TIMEOUT, TimeUnit.MILLISECONDS).dns(dns()).hostnameVerifier((hostname, session) -> true).followRedirects(true).sslSocketFactory(getSSLContext().getSocketFactory(), trustAllCertificates());
         builder.proxySelector(get().proxy ? selector() : defaultSelector);
         return builder;
     }
