4a70be4f9c4483cab6482d1b15eb1a0108a2189d
FongMi <b8892778@gmail.com>
2024-05-08 19:47:24 +0800

    Update XLUtil.java

--- thunder/src/main/java/com/xunlei/downloadlib/android/XLUtil.java
+++ thunder/src/main/java/com/xunlei/downloadlib/android/XLUtil.java
@@ -1,6 +1,6 @@
 package com.xunlei.downloadlib.android;
 
-import java.util.Random;
+import java.security.SecureRandom;
 import java.util.UUID;
 
 public class XLUtil {
@@ -20,7 +20,7 @@ public class XLUtil {
     }
 
     private static String random(String base, int length) {
-        Random random = new Random();
+        SecureRandom random = new SecureRandom();
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < length; i++) sb.append(base.charAt(random.nextInt(base.length())));
         return sb.toString();
