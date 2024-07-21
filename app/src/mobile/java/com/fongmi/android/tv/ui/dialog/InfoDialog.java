4640126c2143e25d4870ab83d95a4f7da49af4d3
FongMi <b8892778@gmail.com>
2024-07-21 17:22:56 +0800

    Fix bug

--- app/src/mobile/java/com/fongmi/android/tv/ui/dialog/InfoDialog.java
+++ app/src/mobile/java/com/fongmi/android/tv/ui/dialog/InfoDialog.java
@@ -64,7 +64,7 @@ public class InfoDialog {
         binding.title.setText(title);
         binding.url.setText(fixUrl());
         binding.header.setText(header);
-        binding.title.setSingleLine(title.equals(url));
+        binding.title.setSingleLine(title.toString().contains(url));
         binding.url.setVisibility(TextUtils.isEmpty(url) ? View.GONE : View.VISIBLE);
         binding.header.setVisibility(TextUtils.isEmpty(header) ? View.GONE : View.VISIBLE);
     }
