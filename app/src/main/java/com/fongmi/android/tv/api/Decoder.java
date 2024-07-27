f495f3e2c000d67a7785d7182aa1ebd25dddd573
FongMi <b8892778@gmail.com>
2024-07-27 17:59:29 +0800

    Clean code

--- app/src/main/java/com/fongmi/android/tv/api/Decoder.java
+++ app/src/main/java/com/fongmi/android/tv/api/Decoder.java
@@ -20,6 +20,8 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class Decoder {
 
+    private static final Pattern JS_URI = Pattern.compile("\"(\\.|\\.\\.)/(.?|.+?)\\.js\\?(.?|.+?)\"");
+
     public static String getJson(String url) throws Exception {
         String key = url.contains(";") ? url.split(";")[2] : "";
         url = url.contains(";") ? url.split(";")[0] : url;
@@ -33,10 +35,21 @@ public class Decoder {
     }
 
     private static String fix(String url, String data) {
-        if (url.startsWith("file") || url.startsWith("assets")) url = UrlUtil.convert(url);
+        url = UrlUtil.convert(url);
+        Matcher matcher = JS_URI.matcher(data);
+        while (matcher.find()) data = replace(url, data, matcher.group());
         if (data.contains("../")) data = data.replace("../", UrlUtil.resolve(url, "../"));
         if (data.contains("./")) data = data.replace("./", UrlUtil.resolve(url, "./"));
+        if (data.contains("__JS1__")) data = data.replace("__JS1__", "./");
+        if (data.contains("__JS2__")) data = data.replace("__JS2__", "../");
         return data;
+    }
+
+    private static String replace(String url, String data, String ext) {
+        String t = ext.replace("\"./", "\"" + UrlUtil.resolve(url, "./"));
+        t = t.replace("\"../", "\"" + UrlUtil.resolve(url, "../"));
+        t = t.replace("./", "__JS1__").replace("../", "__JS2__");
+        return data.replace(ext, t);
     }
 
     public static String getExt(String ext) {
