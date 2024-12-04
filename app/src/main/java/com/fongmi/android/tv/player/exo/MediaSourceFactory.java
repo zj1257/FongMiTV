66b0adbc9549097d712ff162f07661a1fb365131
FongMi <b8892778@gmail.com>
2024-12-04 17:10:56 +0800

    Fix bug

--- app/src/main/java/com/fongmi/android/tv/player/exo/MediaSourceFactory.java
+++ app/src/main/java/com/fongmi/android/tv/player/exo/MediaSourceFactory.java
@@ -85,7 +85,7 @@ public class MediaSourceFactory implements MediaSource.Factory {
     }
 
     private ExtractorsFactory getExtractorsFactory() {
-        if (extractorsFactory == null) extractorsFactory = new DefaultExtractorsFactory().setTsExtractorFlags(FLAG_ALLOW_NON_IDR_KEYFRAMES & FLAG_DETECT_ACCESS_UNITS & FLAG_ENABLE_HDMV_DTS_AUDIO_STREAMS).setTsExtractorTimestampSearchBytes(TsExtractor.DEFAULT_TIMESTAMP_SEARCH_BYTES * 3);
+        if (extractorsFactory == null) extractorsFactory = new DefaultExtractorsFactory().setTsExtractorFlags(FLAG_ALLOW_NON_IDR_KEYFRAMES | FLAG_DETECT_ACCESS_UNITS | FLAG_ENABLE_HDMV_DTS_AUDIO_STREAMS).setTsExtractorTimestampSearchBytes(TsExtractor.DEFAULT_TIMESTAMP_SEARCH_BYTES * 3);
         return extractorsFactory;
     }
 
