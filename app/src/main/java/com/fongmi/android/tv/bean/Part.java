7043ef472ab21517f13e190bb924da4b618da563
FongMi <b8892778@gmail.com>
2024-12-13 22:12:04 +0800

    Fix bug

--- app/src/main/java/com/fongmi/android/tv/bean/Part.java
+++ app/src/main/java/com/fongmi/android/tv/bean/Part.java
@@ -7,7 +7,8 @@ import java.util.List;
 public class Part {
 
     public static List<String> get(String source) {
-        List<String> items = new ArrayList<>(Arrays.asList(source.trim()));
+        List<String> items = new ArrayList<>();
+        items.add(source.trim());
         if (source.contains("：")) {
             for (String split : source.split("：")) items.add(split.trim().contains(" ") ? split.split(" ")[0].trim() : split.trim());
         } else if (source.contains("第") && source.contains("季")) {
