d9586f5e38065a6910a798bdf613d6364be4ffe6
FongMi <b8892778@gmail.com>
2025-01-24 23:34:14 +0800

    Fix bug

--- app/src/main/java/com/fongmi/android/tv/ui/custom/CustomSeekView.java
+++ app/src/main/java/com/fongmi/android/tv/ui/custom/CustomSeekView.java
@@ -103,7 +103,9 @@ public class CustomSeekView extends FrameLayout implements TimeBar.OnScrubListen
     }
 
     private void setKeyTimeIncrement(long duration) {
-        if (duration > TimeUnit.MINUTES.toMillis(30)) {
+        if (duration > TimeUnit.HOURS.toMillis(3)) {
+            timeBar.setKeyTimeIncrement(TimeUnit.MINUTES.toMillis(5));
+        } else if (duration > TimeUnit.MINUTES.toMillis(30)) {
             timeBar.setKeyTimeIncrement(TimeUnit.MINUTES.toMillis(1));
         } else if (duration > TimeUnit.MINUTES.toMillis(15)) {
             timeBar.setKeyTimeIncrement(TimeUnit.SECONDS.toMillis(30));
